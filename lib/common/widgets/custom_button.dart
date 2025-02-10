1bf4e76a2d67547015cd96fbec6f975d047f44b4
asmaaa77 <naimaali2447@gmail.com>
2025-02-09 20:42:49 -0800

    button-drt

--- lib/common/widgets/custom_button.dart
+++ lib/common/widgets/custom_button.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
+  
   final VoidCallback onTap;
   final Color? color;
   const CustomButton({
     Key? key,
     required this.text,
+
     required this.onTap,
+
     this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+        
       child: Text(
         text,
         style: TextStyle(
